3a317bbe55276713475b1f613ad2dab863d7096f
Volodymyr Korniychuk <korniychuk49@gmail.com>
2022-07-23 15:30:53 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,11 @@
+
 import SimpleLightbox from "simplelightbox";
+
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
+
+
 // Change code below this line
 
 const galleryEl = document.querySelector('.gallery');
