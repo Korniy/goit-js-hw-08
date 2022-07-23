f4ff4728437511a84b366244e361480b528a3c11
Volodymyr Korniychuk <korniychuk49@gmail.com>
2022-07-23 15:27:29 +0300

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function creatingGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+       return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('');
+};
+
+galleryEl.insertAdjacentHTML('beforeend', creatingGallery(galleryItems));
+
+const lightboxGallery = new SimpleLightbox('.gallery__item', {
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
