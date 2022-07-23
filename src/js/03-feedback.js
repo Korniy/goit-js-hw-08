aa5af398b9b60ae20c29806b475b1964e3706919
Volodymyr Korniychuk <korniychuk49@gmail.com>
2022-07-23 22:12:30 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,6 +4,7 @@ import throttle from 'lodash.throttle';
 const formEl = document.querySelector('.feedback-form');
 const inputEl = document.querySelector('input');
 const messageEl = document.querySelector('textarea');
+
 const KEY = 'feedback-form-state';
 
 formEl.addEventListener('input', throttle(onInputChange, 500));
@@ -14,29 +15,34 @@ fillForm();
 const formData = {}; 
 
 function onInputChange(event) {
+
     formData[event.target.name] = event.target.value;
     localStorage.setItem(KEY, JSON.stringify(formData));
 };
 
 function onFormSubmit(event) {
+
     event.preventDefault();
     formData.email = inputEl.value;
     formData.message = messageEl.value;
+
     console.log(formData);
+
     event.currentTarget.reset();
     localStorage.removeItem(KEY);
 }
 
 function fillForm() {
-    const rawValues = localStorage.getItem(KEY);
-    const valueObject = JSON.parse(rawValues);
+
+    const rawValue = localStorage.getItem(KEY);
+    const valueObject = JSON.parse(rawValue);
    
 if(valueObject) {
-    const savedEmail = valueObject.email;
-    inputEl.value = savedEmail;
+    const saveEmail = valueObject.email;
+    inputEl.value = saveEmail;
 
-    const savedMessage = valueObject.message;
-    messageEl.value = savedMessage;
+    const saveMessage = valueObject.message;
+    messageEl.value = saveMessage;
 };
 };
 
