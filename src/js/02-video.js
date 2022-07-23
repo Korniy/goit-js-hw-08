b07b8b901cda185365eb1fe945a5333811c8c5ed
Volodymyr Korniychuk <korniychuk49@gmail.com>
2022-07-23 18:24:52 +0300

    добавляє 2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+ 
+const throttle = require('lodash.throttle');
+ const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+ 
+const onPlay = function(data) {
+    const time = data.seconds;
+    localStorage.setItem('videoplayer-current-time', time);
+}
+const saveTime = localStorage.getItem("videoplayer-current-time");
+    
+ player.on('timeupdate', throttle(onPlay, 1000));
+    if(saveTime) {
+        player.setCurrentTime(saveTime);
+    }
 
+    
+ 
