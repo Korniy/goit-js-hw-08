00f5860469a80fbb488a4609eb3da214c20aab28
Volodymyr Korniychuk <korniychuk49@gmail.com>
2022-07-23 18:39:05 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,16 +6,16 @@ const iframeEl = document.querySelector('iframe');
 const player = new Player(iframeEl);
 const KEY = 'videoplayer-current-time';
  
-const onPlay = function(data) {
+const onPlay = function (data) {
     const time = data.seconds;
     localStorage.setItem(KEY, time);
-}
+};
 const saveTime = localStorage.getItem(KEY);
     
  player.on('timeupdate', throttle(onPlay, 1000));
     if(saveTime) {
         player.setCurrentTime(saveTime);
-    }
+};
 
     
  
